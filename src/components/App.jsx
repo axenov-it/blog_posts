aed57098c273c1002bd900b643072965826b2420
alex0735070005 <alex0735070005@gmail.com>
2020-08-01 12:09:45 +0300

    start project

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./static/Header";
+import Footer from "./static/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Home />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/pages/About/index.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./styles.css";
+
+function About() {
+  return (
+    <div>
+      <h1 className="about-title">About</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident ipsa
+        libero reiciendis cumque! Animi mollitia alias sed, neque delectus sit
+        quos modi est repellendus debitis reiciendis quaerat ratione atque
+        itaque!
+      </p>
+    </div>
+  );
+}
+
+export default About;
