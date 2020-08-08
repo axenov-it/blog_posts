a3326b5732ef226545b421549dc2de138520e429
alex0735070005 <alex0735070005@gmail.com>
2020-08-08 12:09:12 +0300

    add styles

--- src/components/pages/Posts/components/Post/index.jsx
+++ src/components/pages/Posts/components/Post/index.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { getDateFromLocale } from "../../../../../lib/helpers";
 import styles from "./styles.module.css";
 
 function Post({ id, title, shortDescription, dateUpdate }) {
+  const { post, post__title, post__desctiption } = styles;
+
   return (
-    <div className={styles.post}>
-      <h2>{title}</h2>
-      <p>{shortDescription}</p>
+    <div className={post}>
+      <span>{getDateFromLocale(dateUpdate.date)}</span>
+      <h2 className={post__title}>{title}</h2>
+      <p className={post__desctiption}>{shortDescription}</p>
     </div>
   );
 }
