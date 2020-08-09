70d0b898e181e6353a549f583fd75f081eefbda2
alex0735070005 <alex0735070005@gmail.com>
2020-08-09 11:23:14 +0300

    add post page

---
+++ src/components/pages/Post/index.jsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+import RequestApi from "../../../lib/RequestApi";
+
+import styles from "./styles.module.css";
+
+function Post(props) {
+  const postId = props.match.params.id;
+
+  const [state, setState] = useState({
+    post: { title: "", full_description: "", short_description: "" },
+  });
+
+  useEffect(() => {
+    RequestApi.getPost(postId).then((data) => {
+      if (data.result) {
+        setState({ ...state, post: data.post });
+      }
+    });
+  }, []);
+
+  const { title, full_description, short_description } = state.post;
+
+  return (
+    <div>
+      <h1 className={styles.post}>{title}</h1>
+      <p>{full_description}</p>
+      <p>{short_description}</p>
+    </div>
+  );
+}
+
+export default Post;
