9da9977ad988502d00666fcae81c07adfca08f41
alex0735070005 <alex0735070005@gmail.com>
2020-08-11 22:16:38 +0300

    add form popup

--- src/components/pages/Posts/index.jsx
+++ src/components/pages/Posts/index.jsx
@@ -3,10 +3,12 @@ import styles from "./styles.module.css";
 import RequestApi from "../../../lib/RequestApi";
 import List from "./components/List";
 import Button from "../../modules/Button";
+import AddPostForm from "./components/AddPostForm";
 
 function Posts() {
   const [state, setState] = useState({
     posts: [],
+    isShowAddPostForm: false,
   });
 
   useEffect(() => {
@@ -17,6 +19,18 @@ function Posts() {
     });
   }, []);
 
+  const onAddFormSubmit = () => {
+    console.log("MY FORM");
+  };
+
+  const onShowForm = () => {
+    setState({ ...state, isShowAddPostForm: !state.isShowAddPostForm });
+  };
+
+  const onCancelForm = () => {
+    setState({ ...state, isShowAddPostForm: false });
+  };
+
   return (
     <div className={styles.posts}>
       <div className={styles.posts__nav}>
@@ -25,11 +39,15 @@ function Posts() {
           type="btn"
           title="Add post"
           className="btn__add"
-          onClick={() => console.log("clicked")}
+          onClick={onShowForm}
         />
       </div>
-
       <List posts={state.posts} />
+      <AddPostForm
+        isShow={state.isShowAddPostForm}
+        onSubmit={onAddFormSubmit}
+        onCancel={onCancelForm}
+      />
     </div>
   );
 }
