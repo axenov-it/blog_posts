a025d217574ac958042507a0bac818f75211354f
alex0735070005 <alex0735070005@gmail.com>
2020-08-11 21:30:03 +0300

    add button components

--- src/components/pages/Posts/index.jsx
+++ src/components/pages/Posts/index.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import styles from "./styles.module.css";
 import RequestApi from "../../../lib/RequestApi";
 import List from "./components/List";
+import Button from "../../modules/Button";
 
 function Posts() {
   const [state, setState] = useState({
@@ -18,7 +19,16 @@ function Posts() {
 
   return (
     <div className={styles.posts}>
-      <h1>Blog posts</h1>
+      <div className={styles.posts__nav}>
+        <h1>Blog posts</h1>
+        <Button
+          type="btn"
+          title="Add post"
+          className="btn__add"
+          onClick={() => console.log("clicked")}
+        />
+      </div>
+
       <List posts={state.posts} />
     </div>
   );
